45f57a2faee814be22da984c1192c0618c7625ee
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-01-18 10:52:46 +0000

    s

--- APIEXTbd.py
+++ APIEXTbd.py
@@ -1410,13 +1410,15 @@ try:
 
                                                     if not other_df.empty:
                                                         skipped_element = other_df.iat[-1, 2]
+                                                        reason = other_df.iat[-1, 3]
                                                         
                                                     else:
                                                         skipped_element = ''
+                                                        reason = ''
 
                                                     bar.progress((index+1)/len(batch_df))
                                                     with iterate.container():
-                                                        st.info(f"Processing Schedules - {project_name} - **{index+1}** of {len(batch_df)} - **{round(((index+1)/(len(batch_df)))*100, 0)} %**  - (Skipped: {skipped_element} - **{len(other_df)}**)")
+                                                        st.info(f"Processing Schedules - {project_name} - **{index+1}** of {len(batch_df)} - **{round(((index+1)/(len(batch_df)))*100, 0)} %**  - (**{len(other_df)}** Schedules skipped: {skipped_element} - Reason: {reason})")
                                                    
                 
 
